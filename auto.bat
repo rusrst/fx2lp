fba1eee30256abef92ed4db23530b4404d777dbb
rusrst <rusrst@outlook.com>
2022-09-19 00:27:25 +0300

    for test

--- auto.bat
+++ auto.bat
@@ -1,8 +1,8 @@
 cd C:\SDCC\bin  
-sdcc -mmcs51 --model-small -c C:\fx2lp\usb_first_step\main.c -o C:\fx2lp\usb_first_step\out\ --debug
-sdcc -mmcs51 --model-small -c C:\fx2lp\usb_first_step\interrupts.c -o C:\fx2lp\usb_first_step\out\ --debug
-sdcc -mmcs51 --model-small -c C:\fx2lp\usb_first_step\table.c -o C:\fx2lp\usb_first_step\out\ --debug
-sdcc -mmcs51 --model-small  -Wl -bUSB_VECTORS=0x1000 C:\fx2lp\usb_first_step\out\main.rel C:\fx2lp\usb_first_step\out\interrupts.rel C:\fx2lp\usb_first_step\out\table.rel -o C:\fx2lp\usb_first_step\out\first_step.hex
+sdcc -mmcs51 --model-small -c C:\fx2lp\fx2lp\main.c -o C:\fx2lp\fx2lp\out\
+sdcc -mmcs51 --model-small -c C:\fx2lp\fx2lp\interrupts.c -o C:\fx2lp\fx2lp\out\ 
+sdcc -mmcs51 --model-small -c C:\fx2lp\fx2lp\table.c -o C:\fx2lp\fx2lp\out\ 
+sdcc -mmcs51 --model-small  -Wl -bUSB_VECTORS=0x1000 C:\fx2lp\fx2lp\out\main.rel C:\fx2lp\fx2lp\out\interrupts.rel C:\fx2lp\fx2lp\out\table.rel -o C:\fx2lp\fx2lp\out\first_step.hex
 cd C:\fx2lp\fx2lp\console\fx2lp_fix_interrupt
-main -in C:\fx2lp\usb_first_step\out\first_step.hex -data 0x021000 -adr 0x43
+main -in C:\fx2lp\fx2lp\out\first_step.hex -data 0x021000 -adr 0x43
 pause
